3bb16f1dd04fd064b763625783cfb410b2df667e
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-05 20:36:52 +0300

    add casting

---
+++ intro.py
@@ -0,0 +1,36 @@
+#Python Indentation
+if 5 > 2:
+    print("Five is greater than two!")
+
+#Python Variables
+x = 5
+y = "Hello, World!"
+
+print(x)
+print(y)
+
+#Python Comments
+#This is a comment
+print("Hello, World!") #This is a comment
+
+#Python Multi Line Comments
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+#Python Variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4 # x is of type int
+x = "Sally" # x is now of type str 
+print(x)
+
+#Casting
+x = str(3) # x will be '3'
+y = int(3) # y will be 3
