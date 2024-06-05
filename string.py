67d0e51911c9f5dbb269343143a9ff8feaf3f5d8
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-05 20:48:30 +0300

    add split method

--- string.py
+++ string.py
@@ -15,3 +15,15 @@ print(len(a))
 # String Methods
 a = " Hello, World! "
 print(a.strip()) # returns "Hello, World!"
+
+a = "Hello, World!"
+print(a.lower())
+
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.replace("H", "J"))  # returns Jello, World!
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
