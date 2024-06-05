5efd3c4228ca81b4c08540d7d64d3e2e9731520a
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-05 21:03:21 +0300

    add escape method

--- string.py
+++ string.py
@@ -58,3 +58,14 @@ itemno = 567
 price = 49.95
 myorder = "I want {} pieces of item {} for {} dollars."
 print(myorder.format(quantity, itemno, price))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
+
+# Escape Character
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
+
